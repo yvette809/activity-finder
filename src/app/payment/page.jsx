2289a0c9063835edc87a2900246b9f16dd74a7e5
yvette809 <60625477+yvette809@users.noreply.github.com>
2024-03-19 11:59:46 +0100

    fixes

--- src/app/payment/page.jsx
+++ src/app/payment/page.jsx
@@ -1,4 +1,4 @@
-/* "use client";
+"use client";
 
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
@@ -30,6 +30,11 @@ const page = () => {
     fetchData();
   }, []);
 
+  if (!activity) {
+    // Render loading state until activity data is fetched
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100 py-[2rem]">
       <div className="max-w-md p-8 bg-white shadow-md rounded-md">
@@ -54,66 +59,3 @@ const page = () => {
 };
 
 export default page;
- */
-
-"use client";
-
-import { useRouter } from "next/navigation";
-import { useSearchParams } from "next/dist/client/router"; // Make sure you import from 'next/dist/client/router'
-import { useState, useEffect } from "react";
-import { getActivity } from "@/utils/api";
-import { formatTime } from "@/utils/formatTime";
-import CreditCardForm from "../components/CreditCardForm";
-
-const Page = () => {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const numberOfPersons = searchParams.get("numberOfPersons");
-  const activityId = searchParams.get("activityId");
-  const bookingStatus = searchParams.get("bookingStatus");
-  const timeSlot = searchParams.get("selectedTimeSlot");
-  const [activity, setActivity] = useState(null); // Initialize activity as null
-
-  const total = activity ? activity.price * numberOfPersons : 0; // Access activity.price only when activity is available
-  const formattedTimeSlot = formatTime(timeSlot);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const fetchedActivity = await getActivity(activityId);
-      setActivity(fetchedActivity);
-    };
-
-    fetchData();
-  }, []);
-
-  if (!activity) {
-    // Render loading state until activity data is fetched
-    return <div>Loading...</div>;
-  }
-
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100 py-[2rem]">
-      <div className="max-w-md p-8 bg-white shadow-md rounded-md">
-        <h1 className="text-2xl font-bold mb-6">Booking Details</h1>
-        <div className="mb-4">
-          <p className="text-gray-700">
-            You are booked for: {activity.typeOfActivity}
-          </p>
-          <p className="text-gray-700">Date: {formattedTimeSlot.date}</p>
-          <p className="text-gray-700">Time: {formattedTimeSlot.time}</p>
-          <p className="text-gray-700">Total: ${total}</p>
-          <p className="text-gray-700">Number of persons: {numberOfPersons}</p>
-        </div>
-        <div className="payment">
-          <CreditCardForm
-            activityId={activityId}
-            price={activity.price}
-            persons={numberOfPersons}
-          />
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default Page;
