be6e4b809de227845798777b6d13f2f7e74e1240
yvette809 <nchombuayvta@gmail.com>
2024-05-20 23:44:45 +0200

    base url changed

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,7 @@
 import { toast } from "react-hot-toast";
 
-const base_url = "http://localhost:3000";
+/* const base_url = "http://localhost:3000"; */
+const base_url = "https://activity-finder-nyjc.vercel.app"
 
 // get all activities
 export async function getActivities() {
